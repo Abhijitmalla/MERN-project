01c33079d85f2e1885395354b4ba61dff2004a81
Abhijitmalla <abhijitmalla06@gmail.com>
2024-12-23 13:55:36 +0530

    MERN-project

---
+++ MERN exam/hello-react/hello-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import Hello from './Hello.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Hello/>
+</div>
+    </>
+  )
+}
+
+export default App
